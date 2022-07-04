e2a4814524502d981a0a7ccc8868cd14ea03529e
Jeeann <jeeann.kim@gmail.com>
2022-07-04 14:53:58 +0100

    3.Get_api_topics

---
+++ __tests__/app.test.js
@@ -0,0 +1,42 @@
+const seed = require("../db/seeds/seed");
+const testData = require("../db/data/test-data");
+const db = require("../db");
+const request = require("supertest");
+const app = require("../app");
+
+beforeEach(() => {
+  return seed(testData);
+});
+
+afterAll(() => {
+  return db.end();
+});
+
+
+describe('News app', () => {
+   describe('3.Get/api/topics', () => {
+    it('200: return all topics', () => {
+      return request(app)
+      .get('/api/topics')
+      .expect(200)
+      .then(({ body: { topics } }) => {
+        topics.forEach((topic) => {
+            console.log(topics);
+          expect(topic).toHaveProperty("description");
+          expect(topic).toHaveProperty("slug");
+        });
+      });
+   }); 
+});
+
+   describe("Sad paths :( ", () => {
+    it("404 for invalid paths", () => {
+      return request(app)
+        .get("/api/topicss")
+        .expect(404)
+        .then(({ body: { msg } }) => {
+          expect(msg).toBe("Invalid Path");
+        });
+    });
+  });
+});
