26b4e0c7d3b4df730760b0a3727771680c9a102e
Jeeann <jeeann.kim@gmail.com>
2022-07-04 14:54:44 +0100

    3.Get_api_topics

--- app.js
+++ app.js
@@ -4,3 +4,23 @@ const { getTopics } = require("./controllers/controller");
 const app = express();
 
 app.get('/api/topics', getTopics);
+
+app.use(express.json());
+
+app.use("*", (req, res) => {
+  res.status(404).send({ msg: "Invalid Path" });
+});
+
+app.use((err, req, res, next) => {
+    if (typeof err === "string") {
+      res.status(400).send({ msg: err });
+    } else next(err);
+  });
+  
+  app.use((err, req, res, next) => {
+    console.log(err);
+    res.status(500).send({ msg: "Server Error" });
+  });
+
+  
+  module.exports = app;
