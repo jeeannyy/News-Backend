a5b0ff0fc8ccc3fb89a8e5d2ad703d4db939e44c
Jeeann <jeeann.kim@gmail.com>
2022-12-22 20:11:17 +0900

    edit connection.js

--- db/connection.js
+++ db/connection.js
@@ -9,16 +9,6 @@ if (!process.env.PGDATABASE && !process.env.DATABASE_URL) {
   throw new Error('PGDATABASE or DATABASE_URL not set');
 }
 
-// const config =
-//   ENV === 'production'
-//     ? {
-//         connectionString: process.env.DATABASE_URL,
-//         ssl: {
-//           rejectUnauthorized: false,
-//         },
-//       }
-//     : {};
-
 const config =
   ENV === 'production'
     ? {
